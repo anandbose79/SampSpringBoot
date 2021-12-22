d4c37144aa292b44654e9d5c18c1a190809ae39f
anandbose79 <anand.bose@gmail.com>
2021-12-22 09:34:40 -0600

    Added basic project to git

---
+++ src/main/java/com/sample/ip/springsamples/SpringsamplesApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ip.springsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/ip/springsamples/SpringsamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ip.springsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
